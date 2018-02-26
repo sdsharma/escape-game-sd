8b51c91a048e3562503287e909ef7e7d582425f9
Shivam Sharma <sdsharma@ucsd.edu>
2018-02-26 10:54:43 -0800

    merge sources

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 /*global jQuery:false */
+(function($) {
+    $(window).load(function() {
+        "use strict";
+
+/*-----------------------------------------------------------------------------------*/
+/*  1. PRELOADING ANIMATION
+/*-----------------------------------------------------------------------------------*/
+
+        $('#loading-status').fadeOut(); 
+        $('#loader').fadeOut('fast');
+        $('body').css({'overflow':'visible'});
+         setTimeout(function(){$('#main-wrap').removeClass('nopreload')},200);
+
+    });
+})(jQuery);
+/*global jQuery:false */
 jQuery(document).ready(function ($) {
     "use strict";
 
