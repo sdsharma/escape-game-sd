0f91986bf3eedb5e852543609b6e07c0396ee351
Shivam Sharma <sdsharma@ucsd.edu>
2018-06-09 00:37:24 -0700

    Second pass optimizations

--- js/accordian.js
+++ js/accordian.js
@@ -1,17 +1,1 @@
-window.onload = function() {
-      "use strict";
-      var acc = document.getElementsByClassName("accordion");
-      var i;
-
-      for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-          this.classList.toggle("active");
-          var panel = this.nextElementSibling;
-          if (panel.style.maxHeight){
-            panel.style.maxHeight = null;
-          } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-          } 
-        });
-      }
-    };
+window.onload=function(){"use strict";var t,e=document.getElementsByClassName("accordion");for(t=0;t<e.length;t++)e[t].addEventListener("click",function(){this.classList.toggle("active");var t=this.nextElementSibling;t.style.maxHeight?t.style.maxHeight=null:t.style.maxHeight=t.scrollHeight+"px"})};
